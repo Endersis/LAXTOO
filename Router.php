ec3457c2808013063050ec581b1b16f0ff0f456d
Endersis <alexisgt7168@gmail.com>
2023-01-05 19:48:33 -0600

    deployment de la app

--- Router.php
+++ Router.php
@@ -19,7 +19,7 @@ class Router
         $currentUrl = $_SERVER['REQUEST_URI'] === '' ? '/' : $_SERVER
         ['REQUEST_URI'];
         $method = $_SERVER['REQUEST_METHOD'];
-
+        $splitURL = explode('?', $currentUrl);
         if ($method === 'GET') {
             $fn = $this->getRoutes[$currentUrl] ?? null;
         } else {
